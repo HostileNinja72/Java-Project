bec24d6e25c3546abb3dfb810a84e67713e5176a
Soufiane47 <soufianeastronomie@gmail.com>
2022-01-09 01:47:50 +0100

    Pane Switching1

--- src/controllers/HomeController.java
+++ src/controllers/HomeController.java
@@ -3,9 +3,15 @@ package controllers;
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
 
 public class HomeController {
 
+    @FXML
+    AnchorPane AccPane;
     @FXML
     private JFXButton AcceuilBotton;
 
@@ -30,11 +36,15 @@ public class HomeController {
     @FXML
     void acceuil(ActionEvent event) {
 
+
     }
 
     @FXML
-    void calendrier(ActionEvent event) {
-
+    void calendrier(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/interfaces/calendrier.fxml"));
+        AnchorPane CalPane = (AnchorPane) fxmlLoader.load();
+        AccPane.getChildren().clear();
+        AccPane.getChildren().add(CalPane);
     }
 
     @FXML
